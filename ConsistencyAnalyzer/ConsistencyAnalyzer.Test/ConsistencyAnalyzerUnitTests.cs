b3431d82e7014dc872f70b94587c0ef14e11b29b
dlebansais <dlebansais@gmail.com>
2021-01-10 16:55:53 +0100

    Add project files.

---
+++ ConsistencyAnalyzer/ConsistencyAnalyzer.Test/ConsistencyAnalyzerUnitTests.cs
@@ -0,0 +1,270 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = ConsistencyAnalyzer.Test.CSharpCodeFixVerifier<
+    ConsistencyAnalyzer.ConsistencyAnalyzerAnalyzer,
+    ConsistencyAnalyzer.ConsistencyAnalyzerCodeFixProvider>;
+
+namespace ConsistencyAnalyzer.Test
+{
+    [TestClass]
+    public class ConsistencyAnalyzerUnitTest
+    {
+        private const string LocalIntCouldBeConstant = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string LocalIntCouldBeConstantFixed = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        [DataTestMethod]
+        [DataRow(""),
+         DataRow(VariableAssigned),
+         DataRow(AlreadyConst),
+         DataRow(NoInitializer),
+         DataRow(InitializerNotConstant),
+         DataRow(MultipleInitializers),
+         DataRow(DeclarationIsInvalid),
+         DataRow(ReferenceTypeIsntString)]
+        public async void WhenTestCodeIsValidNoDiagnosticIsTriggered(string testCode)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(testCode);
+        }
+
+        [DataTestMethod]
+        [DataRow(LocalIntCouldBeConstant, LocalIntCouldBeConstantFixed, 10, 13),
+         DataRow(ConstantIsString, ConstantIsStringFixed, 10, 13),
+         DataRow(DeclarationUsesVar, DeclarationUsesVarFixedHasType, 10, 13),
+         DataRow(StringDeclarationUsesVar, StringDeclarationUsesVarFixedHasType, 10, 13)]
+        public async void WhenDiagosticIsRaisedFixUpdatesCode(
+                    string test,
+            string fixTest,
+            int line,
+            int column)
+        {
+            var descriptor = new DiagnosticDescriptor(
+                ConsistencyAnalyzerAnalyzer.DiagnosticId, 
+                "title", 
+                new LocalizableResourceString(nameof(ConsistencyAnalyzer.Resources.AnalyzerMessageFormat), ConsistencyAnalyzer.Resources.ResourceManager, typeof(ConsistencyAnalyzer.Resources)).ToString(),
+                "description",
+                DiagnosticSeverity.Warning,
+                true
+                );
+
+            var expected = new DiagnosticResult(descriptor);
+            expected = expected.WithLocation("Test0.cs", line, column);
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        private const string VariableAssigned = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine(i++);
+        }
+    }
+}";
+
+        private const string AlreadyConst = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string NoInitializer = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string InitializerNotConstant = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = DateTime.Now.DayOfYear;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string MultipleInitializers = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j = DateTime.Now.DayOfYear;
+            Console.WriteLine(i, j);
+        }
+    }
+}";
+
+        private const string DeclarationIsInvalid = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = ""abc"";
+        }
+    }
+}";
+
+        private const string ReferenceTypeIsntString = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object s = ""abc"";
+        }
+    }
+}";
+
+        private const string ConstantIsString = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ""abc"";
+        }
+    }
+}";
+
+        private const string ConstantIsStringFixed = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string s = ""abc"";
+        }
+    }
+}";
+
+        private const string DeclarationUsesVar = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = 4;
+        }
+    }
+}";
+
+        private const string DeclarationUsesVarFixedHasType = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int item = 4;
+        }
+    }
+}";
+        private const string StringDeclarationUsesVar = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = ""abc"";
+        }
+    }
+}";
+        private const string StringDeclarationUsesVarFixedHasType = @"
+using System;
+
+namespace ConsistencyAnalyzerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string item = ""abc"";
+        }
+    }
+}";
+    }
+}
---
+++ ConsistencyAnalyzer/ConsistencyAnalyzer/ConsistencyAnalyzerAnalyzer.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace ConsistencyAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConsistencyAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ConsistencyAnalyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+
+            // make sure the declaration isn't already const:
+            if (localDeclaration.Modifiers.Any(SyntaxKind.ConstKeyword))
+            {
+                return;
+            }
+
+            var variableTypeName = localDeclaration.Declaration.Type;
+            var variableType = context.SemanticModel.GetTypeInfo(variableTypeName).ConvertedType;
+
+            // Ensure that all variables in the local declaration have initializers that
+            // are assigned with constant values.
+            foreach (var variable in localDeclaration.Declaration.Variables)
+            {
+                var initializer = variable.Initializer;
+                if (initializer == null)
+                {
+                    return;
+                }
+
+                var constantValue = context.SemanticModel.GetConstantValue(initializer.Value);
+                if (!constantValue.HasValue)
+                {
+                    return;
+                }
+
+                // Ensure that the initializer value can be converted to the type of the
+                // local declaration without a user-defined conversion.
+                var conversion = context.SemanticModel.ClassifyConversion(initializer.Value, variableType);
+                if (!conversion.Exists || conversion.IsUserDefined)
+                {
+                    return;
+                }
+
+                // Special cases:
+                //  * If the constant value is a string, the type of the local declaration
+                //    must be System.String.
+                //  * If the constant value is null, the type of the local declaration must
+                //    be a reference type.
+                if (constantValue.Value is string)
+                {
+                    if (variableType.SpecialType != SpecialType.System_String)
+                    {
+                        return;
+                    }
+                }
+                else if (variableType.IsReferenceType && constantValue.Value != null)
+                {
+                    return;
+                }
+            }
+
+            // Perform data flow analysis on the local declaration.
+            var dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(localDeclaration);
+
+            foreach (var variable in localDeclaration.Declaration.Variables)
+            {
+                // Retrieve the local symbol for each variable in the local declaration
+                // and ensure that it is not written outside of the data flow analysis region.
+                var variableSymbol = context.SemanticModel.GetDeclaredSymbol(variable);
+                if (dataFlowAnalysis.WrittenOutside.Contains(variableSymbol))
+                {
+                    return;
+                }
+            }
+
+            foreach (var variable in localDeclaration.Declaration.Variables)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation(), variable.Identifier.ValueText));
+            }
+        }
+    }
+}
